235a326d4367ede5000735b6722277df2eaa4e1c
“zkelly24” <zkelly24@gmail.com>
2025-09-15 17:18:34 +0200

    Update to copy

--- views/home.js
+++ views/home.js
@@ -25,13 +25,13 @@ function home (state, emit) {
       <main class="View-container View-container--nudge">
         <section>
             <h1 class="Display ${animate ? 'u-slideIn' : ''} Display--intro u-spaceIntro u-spaceIntro--alt" style="${animate ? 'animation-delay: 100ms;' : ''}">
-              ${text`(I'm`} 
+              ${text`I'm`} 
               <a class="View-link--intro "href="https://itszoes.site/about" target="_blank">${text`Zoe Kelly`}</a>
               ${text`and I work with UX in Stockholm. I believe in work that is`} 
               <a class="View-link--intro "href="https://humanebydesign.com/" target="_blank">${text`humane`}</a>
               ${text`&`} 
               <a class="View-link--intro " href="http://5a5f89b8e10a225a44ac-ccbed124c38c4f7a3066210c073e7d55.r9.cf1.rackcdn.com/files/pdfs/news/Empathy_on_the_Edge.pdf" target="_blank">${text`empathetic`}</a>
-              ${text`by design.)`}
+              ${text`by design.`}
             </h1>
           
           <div class="View-grid" id="cases">
